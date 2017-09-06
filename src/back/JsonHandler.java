532e7eb83781a10dddaa4659d8a6e0833dbc86fb
brenocg29 <gabrichbreno@gmail.com>
2017-09-05 22:14:01 -0300

    Class JsonHandler initiated

--- src/back/JsonHandler.java
+++ src/back/JsonHandler.java
@@ -1,5 +1,13 @@
 package back;
 import org.json.*;
+import back.Comrade;
 public class JsonHandler {
-	
+	private String pathToFile;
+	public boolean writeComrade(Comrade newcom) {
+		//todo: method that takes a comrade and write it in a file
+		return true;
+	}
+	public boolean ReadComrade(){
+		return true;
+	}
 }
