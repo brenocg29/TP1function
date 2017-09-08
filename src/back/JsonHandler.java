34929f18f39327d1322a1817b96764bffd098c21
brenocg29 <gabrichbreno@gmail.com>
2017-09-08 16:58:46 -0300

    attempt of a java docs on group and hadler

--- src/back/JsonHandler.java
+++ src/back/JsonHandler.java
@@ -1,30 +1,46 @@
 package back;
 import org.json.*;
-import back.Comrade;
+import back.*;
+/**
+ * Class to handle all writing to json
+ * @author Breno Chaves Gabrich
+ * @author Gabriel Pires
+ * @author Fernanda Ramalho
+ */
 public class JsonHandler {
 	private String pathToFile;
-	public boolean writeComrade(Comrade newcom) {
-		//todo: method that takes a comrade and write it in a file
+	/**
+	 * 
+	 * @param X Write an object to json, the object can be an comrade a page a post or a group
+	 * @return result of operation
+	 */
+	public boolean SaveJson(Comrade Com){
 		return true;
 	}
-	public boolean ReadComrade(){
+	public boolean SaveJson(Group G) {
 		return true;
 	}
-	public boolean newPage() {
+	public boolean SaveJson(Page P) {
 		return true;
 	}
-	public boolean read_pages() {
+	public boolean SaveJson(Post P) {
 		return true;
 	}
-	public boolean read_pageData(String name) {
+	/**
+	 * 
+	 * @param X Read from json, the object can be a comrade page, group or post
+	 * @return result of operation
+	 */
+	public boolean readFromJson(Group G) {
 		return true;
 	}
-	public boolean readFeed(String personName) {
+	public boolean readFromJson(Comrade Com) {
 		return true;
 	}
-	public boolean newGroup(// input a group object after writing this class
-			){
+	public boolean readFromJson(Page P) {
 		return true;
 	}
-	
+	public boolean readFromJson(Post P) {
+		return true;
+	}
 }
