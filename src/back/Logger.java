613061b6e6f12399e99f9c29193c3c593669b7c5
brenocg29 <gabrichbreno@gmail.com>
2017-09-08 17:07:47 -0300

    javadocs on logger

--- src/back/Logger.java
+++ src/back/Logger.java
@@ -1,21 +1,44 @@
 package back;
 import back.Comrade;
 import back.JsonHandler;
+/**
+ * Handles login operation
+ * @author Breno Chaves Gabrich
+ * @author Gabriel Pires
+ * @author Fernada Ramalho
+ *
+ */
 public class Logger {
 	private String user;
 	private String pass;
+	/**
+	 * Construct the object with parameters
+	 * @param user username given by user
+	 * @param pass password given by user
+	 */
 	Logger(String user, String pass){
 		this.user = user;
 		this.pass = pass;
 	}
+	/**
+	 * Check data and returns comrade object
+	 * @return
+	 */
 	public Comrade CheckUserPass() {
 		Comrade X = new Comrade("","","","","");
 		return X;
 	}
+	/**
+	 * Handles a situation where user forgot his password
+	 */
 	public void ForgotPass() {
 		
 	}
-	public void changeUserPass() {
+	/**
+	 * Handles the situation where user wants to change its password
+	 * @param NewPass new password
+	 */
+	public void changeUserPass(String NewPass) {
 		
 	}
 }
