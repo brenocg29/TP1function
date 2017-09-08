9fbe01632f1f03a71698028ab9aaaf6a465ec8ad
brenocg29 <gabrichbreno@gmail.com>
2017-09-08 17:22:38 -0300

    javadocs on logger

--- src/back/Page.java
+++ src/back/Page.java
@@ -1,17 +1,27 @@
 package back;
 import back.Comrade;
 import back.JsonHandler;
+/**
+ * Represents a page
+ * @author Breno Chaves Gabrich
+ * @author Gabriel Pires
+ * @author Fernanda Ramalho
+ *
+ */
 public class Page {
 	private String name;
 	private String numberFollowers;
 	private Comrade Owner;
+	/**
+	 * Constructs a page
+	 * @param name
+	 * @param NumberFollowers
+	 * @param owner
+	 */
 	Page(String name, String NumberFollowers,Comrade owner){
 		this.name = name;
 		this.numberFollowers = NumberFollowers;
 		this.Owner = owner;
-	}
-	Page(){
-		
 	}
 	public void MakePost() {
 		
